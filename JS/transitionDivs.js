3b6adab301fde74e44ba44da485a3b72354103ba
Ringo <59732670+RingoOrigo@users.noreply.github.com>
2024-11-16 14:26:47 -0500

    I accidentally updated the wrong files..
    whoops

--- JS/transitionDivs.js
+++ JS/transitionDivs.js
@@ -5,13 +5,15 @@ function transition() {
     const audio = document.getElementById("audioEmbed");
 
     // Enable the title screen, then fade the loading screen out and play the audio.
-    titleScreen.style.display = "flex";
     loadingScreen.classList.add("fadeOut");
+    titleScreen.classList.add("fadeIn");
+    titleScreen.style.display = "flex";
     audio.play();
 
     // Make absolutely sure that the fading animation finishes before removing the loading screen.
     setTimeout(() => {
         loadingScreen.style.display = "none";
+        titleScreen.classList.remove("fadeIn");
     }, 900);
 }
 
@@ -53,6 +55,39 @@ function setTheme (index) {
         "url('https://i.imgur.com/nvvfGnu.jpeg'"
     ];
 
-    document.body.style.backgroundImage = themeURLs[index];
+    container = document.getElementById("container");
+    buffer = document.getElementById("containerBuffer");
+
+    // Okay this is a clusterf*ck of code genuinely
+    // So basically what I am doing is swapping between an active div and a buffer so that
+    //      there can be a cute fade transition when changing themes.
+
+    if (buffer.style.display == "none") {
+        // If the buffer is not currently displayed, update the buffer's background
+        // Then fade out the main container and fade in the buffer.
+        buffer.style.backgroundImage = themeURLs[index];
+        container.classList.add("fadeOut");
+        buffer.classList.add("fadeIn");
+        buffer.style.display = "flex";
+
+        setTimeout(() => {
+            // Remove any fade animations that are left on the divs and fully hide the container
+            container.style.display = "none";
+            container.classList.remove("fadeOut");
+            buffer.classList.remove("fadeIn");
+        }, 1000);
+    } else {
+        // If the buffer is actively displayed, then do the exact same process but swap the buffer with the container.
+        container.style.backgroundImage = themeURLs[index];
+        buffer.classList.add("fadeOut");
+        container.classList.add("fadeIn");
+        container.style.display = "flex";
+
+        setTimeout(() => {
+            buffer.style.display = "none";
+            buffer.classList.remove("fadeOut");
+            container.classList.remove("fadeIn");
+        }, 1000);
+    }
     closeMenu();
 }
